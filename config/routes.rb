2db4363d2e4ef4882c96a57c74021c42253278df
Ryan Searls <Ryan.searls@gmail.com>
2021-09-22 12:29:45 -0600

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/shows" => "shows#index"
+  post "/shows" => "shows#create"
+  get "/shows/:id" => "shows#show"
+  patch "/shows/:id" => "shows#update"
+  delete "/shows/:id" => "shows#destroy"
+
+  get "/actors" => "actors#index"
+  post "/actors" => "actors#create"
+  get "/actors/:id" => "actors#show"
+  patch "/actors/:id" => "actors#update"
+  delete "/actors/:id" => "actors#destroy"
+
+  get "/users" => "users#index"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+ 
 end
