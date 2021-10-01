7b3121320ada6efbdaa89ac7bfab9270abf4d348
Ryan Searls <Ryan.searls@gmail.com>
2021-10-01 11:29:59 -0600

    seeds datat

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Show.create!([
-#   { title: "Breaking Bad", year: 2008, description: "Teacher cooks meth because he has cancer!", creator: "Vince Gilligan", seasons: 5, network: "AMC", favorite: true, image: "image.jpg" },
-#   { title: "Family Guy", year: 1999, description: "Lucky there's a man who! Positively can do!", creator: "Seth MacFarlane", seasons: 19, network: "Fox", favorite: false, image: "image.jpg" },
-#   { title: "Ted Lasso", year: 2020, description: "American wanka goes across the pond to coach some footie", creator: "Bill Lawrence", seasons: 2, network: "Apple TV+", favorite: true, image: "image.jpg" },
-#   { title: "Succession", year: 2018, description: "Wealthy family members vie for control of family company and wealth.", creator: "Jesse Armstrong", seasons: 2, network: "HBO", favorite: true, image: "image.jpg" },
-#   { title: "Big Bang Theory", year: 2007, description: "Nerds scared of women. Lack social awareness. Bang Bang.", creator: "Chuck Lorre", seasons: 12, network: "CBS", favorite: false, image: "image.jpg" },
-#   { title: "Friends", year: 1994, description: "NYC homies joke at coffee shop but it's whatever.", creator: "David Crane", seasons: 10, network: "NBC", favorite: false, image: "image.jpg" },
-# ])
+Show.create!([
+  { title: "Breaking Bad", year: 2008, description: "Teacher cooks meth because has cancer!", creator: "Vince Gilligan", seasons: 5, network: "AMC", favorite: true, user_id: 1, image: "https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg" },
+  { title: "Family Guy", year: 1999, description: "Lucky there's a man who! Positively can do!", creator: "Seth MacFarlane", seasons: 19, network: "Fox", favorite: false, user_id: 1, image: "https://m.media-amazon.com/images/M/MV5BODEwZjEzMjAtNjQxMy00Yjc4LWFlMDAtYjhjZTAxNDU3OTg3XkEyXkFqcGdeQXVyOTM2NTM4MjA@._V1_.jpg" },
+  { title: "Ted Lasso", year: 2020, description: "American wanka goes across the pond to coach some footie", creator: "Bill Lawrence", seasons: 2, network: "Apple TV+", favorite: true, user_id: 1, image: "https://www.apple.com/tv-pr/shows-and-films/t/ted-lasso/images/show-home-graphic-header/Apple_TV_Ted_Lasso_key_art_sh_cr.jpg" },
+  { title: "Succession", year: 2018, description: "Wealthy family members vie for control of family company and wealth.", creator: "Jesse Armstrong", seasons: 2, network: "HBO", favorite: true, user_id: 1, image: "https://images-na.ssl-images-amazon.com/images/I/A1mRQW+wssL._RI_.jpg" },
+  { title: "Big Bang Theory", year: 2007, description: "Nerds scared of women. Lack social awareness. Bang Bang.", creator: "Chuck Lorre", seasons: 12, network: "CBS", favorite: false, user_id: 1, image: "https://www.tvguide.com/a/img/catalog/provider/1/1/1-6482810627.jpg" },
+  { title: "Friends", year: 1994, description: "NYC homies joke at coffee shop but it's whatever.", creator: "David Crane", seasons: 10, network: "NBC", favorite: false, user_id: 1, image: "https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg" },
+  { title: "The Last Man on Earth", year: 2015, description: "Margarita pools are a great way to pass the time.", creator: "Will Forte", seasons: 4, network: "Fox", favorite: true, user_id: 1, image: "https://images-na.ssl-images-amazon.com/images/I/A1W0xWl0tYL._RI_.jpg" },
+])
 
 Actor.create!([
   { name: "Bryan Cranston", age: 65, gender: "Male", hometown: "Hollywood, CA" },
   { name: "Jason Sudeikis", age: 46, gender: "Male", hometown: "Fairfax, VA" },
   { name: "Sarah Snook", age: 33, gender: "Female", hometown: "Adelaide, Australia" },
+  { name: "Seth Green", age: 47, gender: "Male", hometown: "Overbrook Park, PA" },
+  { name: "Kaley Cuoco", age: 35, gender: "Female", hometown: "Camarillo, CA" },
+  { name: "Seth MacFarlane", age: 47, gender: "Male", hometown: "Kent, CT" },
+  { name: "Matthew Perry", age: 52, gender: "Male", hometown: "Williamstown, MA" },
+  { name: "Lisa Kudrow", age: 58, gender: "Female", hometown: "Los Angeles, CA" },
+  { name: "Jennifer Aniston", age: 52, gender: "Female", hometown: "Los Angeles, CA" },
+  { name: "Mayim Bialik", age: 45, gender: "Female", hometown: "San Diego, CA" },
 ])
 
 
@@ -43,8 +51,24 @@ ShowGenre.create!([
   { show_id: 3, genre_id: 7 },
   { show_id: 2, genre_id: 1 },
   { show_id: 2, genre_id: 9 },
-  { show_id: 4, genre_id: 4 },
-  { show_id: 6, genre_id: 6 },
+  { show_id: 4, genre_id: 3 },
+  { show_id: 6, genre_id: 1 },
   { show_id: 6, genre_id: 9 },
   { show_id: 5, genre_id: 9 },
+  { show_id: 7, genre_id: 9 },
+  { show_id: 7, genre_id: 3 }
+])
+
+ActorShow.create!([
+  { actor_id: 1, show_id: 1 },
+  { actor_id: 2, show_id: 3 },
+  { actor_id: 3, show_id: 4 },
+  { actor_id: 4, show_id: 2 },
+  { actor_id: 5, show_id: 5 },
+  { actor_id: 6, show_id: 2 },
+  { actor_id: 7, show_id: 6 },
+  { actor_id: 8, show_id: 6 },
+  { actor_id: 9, show_id: 6 },
+  { actor_id: 2, show_id: 7 },
+  { actor_id: 10, show_id: 5 },
 ])
